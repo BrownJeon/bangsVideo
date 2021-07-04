75d4a25fcedaf111cbb6c3b63e8980d87e7c7e60
전병훈 <ifries8611@gmail.com>
2021-07-04 11:56:56 +0900

    pug파일로 render해주도록 처리 추가

--- src/server.js
+++ src/server.js
@@ -12,6 +12,8 @@ const logger = morgan("dev");
 
 app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/videos", videoRouter);
