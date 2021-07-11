b086f1b60889a5e98a12d2bec4382475329345a6
전병훈 <ifries8611@gmail.com>
2021-07-11 15:57:37 +0900

    login기능 추가 및 session에 유저정보 저장

--- src/controllers/userContoroller.js
+++ src/controllers/userContoroller.js
@@ -54,6 +54,10 @@ export const postLogin = async (req, res) => {
             errorMessage: "비밀번호가 맞지않습니다.",
         });
     }
+
+    req.session.loggedIn = true;
+    req.session.user = user;
+
     return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import session from "express-session";
 
 const app = express();
 
@@ -14,6 +15,26 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: "Hello!",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
+app.get("/add-one", (req, res, next) => {
+    req.session.counter += 1;
+    return res.send(`${req.session.id} ${req.session.counter}`);
+});
 
 app.use("/", rootRouter);
 app.use("/user", userRouter);
