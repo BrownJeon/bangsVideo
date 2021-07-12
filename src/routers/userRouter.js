b36bfb01e41cabb08d2becc9bb0b0c2fb677563e
전병훈 <ifries8611@gmail.com>
2021-07-12 12:44:20 +0900

    github social login추가

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {edit, logout, remove, see} from "../controllers/userContoroller";
+import {edit, finishGithubLogin, logout, remove, see, startGithubLogin} from "../controllers/userContoroller";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/auth/github/start", startGithubLogin);
+userRouter.get("/auth/github/callback", finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
